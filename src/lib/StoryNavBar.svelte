49b3e5e06a8eed922da1b26c9ff13360c6cf9c00
Alef <alefff.gabriel@gmail.com>
2024-02-25 21:00:14 -0300

    add project page to buy

---
+++ src/lib/StoryNavBar.svelte
@@ -0,0 +1,62 @@
+<script>
+</script>
+
+<div class="sticky top-0 w-full h-20 bg-white flex border">
+  <div class="flex w-2/4 gap-4 items-center">
+    <div
+      class=" p-4 flex flex-col w-1/6 gap-1 items-center hover:border-b-2 border-gray-800"
+    >
+      <a href="#">
+        <div id="pic-basic"></div>
+      </a>
+      <p class="text-lg text-gray-600">Campaign</p>
+    </div>
+    <div
+      class="p-4 flex flex-col w-1/6 gap-1 items-center hover:border-b-2 border-gray-800"
+    >
+      <a href="#">
+        <div id="pic-story"></div>
+      </a>
+      <p class="text-lg text-gray-600">FAQ</p>
+    </div>
+    <div
+      class="p-4 flex flex-col w-1/6 gap-1 items-center hover:border-b-2 border-gray-800"
+    >
+      <a href="#">
+        <div id="pic-people"></div>
+      </a>
+      <p class="text-lg text-gray-600">Comments</p>
+    </div>
+	<div
+      class="p-4 flex flex-col w-1/6 gap-1 items-center hover:border-b-2 border-gray-800"
+    >
+      <a href="#">
+        <div id="pic-people"></div>
+      </a>
+      <p class="text-lg text-gray-600">Updates</p>
+    </div>
+  </div>
+  <div class="flex w-2/4 items-center justify-end p-8 gap-8">
+    <button
+      class="bg-violet-600 hover:bg-violet-700 text-white py-2 px-4 w-1/4"
+    >
+      Back this project
+    </button>
+    <button
+      class="bg-white border text-gray-600 py-2 px-4 w-1/4 flex items-center justify-center gap-2"
+    >
+      <div id="pic-remind"></div>
+      Remind me
+    </button>
+  </div>
+</div>
+
+<style>
+  #pic-remind {
+    background-image: url("$lib/images/remind.png");
+    background-position: center;
+    background-size: cover;
+    width: 16px;
+    height: 16px;
+  }
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,1 @@
+<h1>porject page</h1>
