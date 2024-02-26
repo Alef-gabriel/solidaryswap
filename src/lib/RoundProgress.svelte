df8570a8011520aa7901208ee9e9cdc3d6096399
Alef <alefff.gabriel@gmail.com>
2024-02-25 21:01:20 -0300

    add rounded progress bar

---
+++ src/lib/RoundProgress.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  const circumference = 2 * Math.PI * 90;
+  export let porcentage: number;
+</script>
+
+<div class="relative w-16 h-16">
+  <svg class="w-full h-full" viewBox="0 0 100 100">
+    <circle
+      class="text-gray-200 stroke-current"
+      stroke-width="10"
+      cx="50"
+      cy="50"
+      r="40"
+      fill="transparent"
+    ></circle>
+    <circle
+      class="text-indigo-500 progress-ring__circle stroke-current"
+      stroke-width="10"
+      stroke-linecap="round"
+      cx="50"
+      cy="50"
+      r="40"
+      fill="transparent"
+      stroke-dasharray={circumference}
+      stroke-dashoffset={circumference - (porcentage / 100) * circumference}
+    ></circle>
+
+    <text
+      x="50"
+      y="50"
+      font-family="Verdana"
+      font-size="20"
+      text-anchor="middle"
+      alignment-baseline="middle">{porcentage}%</text
+    >
+  </svg>
+</div>
+
+<style>
+  .progress-ring__circle {
+    stroke-dasharray: 400, 400;
+    transition: stroke-dashoffset 0.35s;
+    transform: rotate(-90deg);
+    transform-origin: 50% 50%;
+  }
+</style>
---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import RoundProgress from "$lib/RoundProgress.svelte";
+
+</script>
+
+<RoundProgress porcentage={50}/>
