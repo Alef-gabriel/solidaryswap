7023b43bcc5cf643d5d522128ad0b767ec67fb0a
Alef <alefff.gabriel@gmail.com>
2024-03-02 18:14:59 -0300

    hardhat contract deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,25 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 import hre from "hardhat";
+import { Wallet, getDefaultProvider } from "ethers";
 
-const projectTable = await ethers.deployContract("ProjectTable");
+async function main() {
+  const privateKey =
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const provider = getDefaultProvider("http://127.0.0.1:8545");
+  const wallet = new Wallet(privateKey, provider);
 
-await projectTable.waitForDeployment();
+  const contractFactory = await hre.ethers.getContractFactory(
+    "ProjectTable",
+    wallet
+  );
 
-console.log(
-  `ProjectTable deployed to ${projectTable.target}`
-);
+  const contract = await contractFactory.deploy();
+
+  console.log(`ProjectTable deployed to ${contract.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
