fccf9c2fc872a6fa8cc740d47ac920bde6be3baf
Alef <alefff.gabriel@gmail.com>
2024-03-06 22:52:12 -0300

    add some package and change vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { join } from 'path';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+		  '@': join(__dirname, ''), // Alias for entire src directory
+		},
+	  },
 	plugins: [sveltekit()]
 });
