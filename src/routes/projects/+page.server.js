391ff957bfe1eaf929a42e9514cbaead3d920804
Alef <alefff.gabriel@gmail.com>
2024-03-10 01:01:47 -0300

    fix variables

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -1,18 +1,20 @@
 import { Database } from "@tableland/sdk";
 import { Wallet, getDefaultProvider } from "ethers";
+import { PUBLIC_PROVIDER_URL } from "$env/static/public";
+import {
+  SECRET_WALLET_PRIVATY_KEY,
+  SECRET_PROJECT_TABLE_NAME,
+} from "$env/static/private";
 
 export const load = async () => {
   const fetchProjects = async () => {
-    const privateKey =
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-    const provider = getDefaultProvider("http://127.0.0.1:8545");
-    const wallet = new Wallet(privateKey, provider);
+    const provider = getDefaultProvider(PUBLIC_PROVIDER_URL);
+    const wallet = new Wallet(getEnv(SECRET_WALLET_PRIVATY_KEY), provider);
 
     const signer = wallet.connect(provider);
     const db = new Database({ signer });
 
-    const tableName = "table_attributes_31337_2";
-    const { results } = await db.prepare(`SELECT * FROM ${tableName}`).all();
+    const { results } = await db.prepare(`SELECT * FROM ${SECRET_PROJECT_TABLE_NAME}`).all();
     return results;
   };
 
