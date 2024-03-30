603566424a66627ac9f95c44a1de4388bf272733
Alef <alefff.gabriel@gmail.com>
2024-03-30 19:34:03 -0300

    feat: create project controller

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { ProjectService } from './project.service';
+import { CreateProjectDto } from './createProject.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { UpdateProjectDto } from './updateProject.dto';
 
-@Controller('')
-export class ProjectController {}
+@Controller('project')
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('create')
+  createProject(@Body() createDto: CreateProjectDto) {
+    return this.projectService.createProject(createDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('all')
+  getProjects(@Body() id: string) {
+    return this.projectService.findAll(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('backers')
+  getUser(@Body() contractId: string) {
+    return this.projectService.backersLenght(contractId);
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('story')
+  updateProject(@Body() data: UpdateProjectDto) {
+    return this.projectService.updateProject(data);
+  }
+}
