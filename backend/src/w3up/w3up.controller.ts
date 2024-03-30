2ccf96edf82292cf0d618ccbd38574ec48cbd5cb
Alef <alefff.gabriel@gmail.com>
2024-03-30 11:55:07 -0300

    feat: create backend web3 delegation

--- backend/src/w3up/w3up.controller.ts
+++ backend/src/w3up/w3up.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { W3upService } from './w3up.service';
+import { createDelegationDto } from './createDelegation.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('w3up')
-export class W3upController {}
+export class W3upController {
+  constructor(private w3upService: W3upService) {}
+
+  @UseGuards(AuthGuard)
+  @Post('create')
+  getUser(@Body() createDelegation: createDelegationDto) {
+    return this.w3upService.createDelegation(createDelegation);
+  }
+}
--- backend/src/w3up/w3up.service.ts
+++ backend/src/w3up/w3up.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { CarReader } from '@ipld/car';
+import * as DID from '@ipld/dag-ucan/did';
+import * as Delegation from '@ucanto/core/delegation';
+import * as Signer from '@ucanto/principal/ed25519';
+import * as Client from '@web3-storage/w3up-client';
+import { StoreMemory } from '@web3-storage/w3up-client/stores/memory';
+import { createDelegationDto } from './createDelegation.dto';
 
 @Injectable()
-export class W3upService {}
+export class W3upService {
+  async createDelegation(createDelegation: createDelegationDto) {
+    const principal = Signer.parse(process.env.SECRET_KEY);
+    const store = new StoreMemory();
+    const client = await Client.create({ principal, store });
+    // Add proof that this agent has been delegated capabilities on the space
+    const proof = await this.parseProof(process.env.SECRET_PROOF);
+    const space = await client.addSpace(proof);
+    await client.setCurrentSpace(space.did());
+
+    const audience = DID.parse(createDelegation.did);
+    const abilities = ['store/add', 'upload/add'];
+    const expiration = Math.floor(Date.now() / 1000) + 60 * 3; // 3 minutes from now
+    const delegation = await client.createDelegation(audience, abilities, {
+      expiration,
+    });
+
+    const archive = await delegation.archive();
+    return archive.ok;
+  }
+  /** @param {string} data Base64 encoded CAR file */
+  async parseProof(data) {
+    const blocks = [];
+    const reader = await CarReader.fromBytes(Buffer.from(data, 'base64'));
+    for await (const block of reader.blocks()) {
+      blocks.push(block);
+    }
+    return Delegation.importDAG(blocks);
+  }
+}
