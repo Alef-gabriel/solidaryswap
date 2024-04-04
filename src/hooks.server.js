50e1bea68abf87339ff53b2dd45e4b0a4de5c37a
Alef <alefff.gabriel@gmail.com>
2024-04-04 19:59:53 -0300

    fix: user come from backend, improve performance

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import { SECRET_INGREDIENT } from "$env/static/private";
-import  { findUserById}  from "$lib/findUserBy.js";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
@@ -10,8 +9,7 @@ export async function handle({ event, resolve }) {
     const claims = jwt.verify(authToken, SECRET_INGREDIENT);
     if (!claims) event.locals.authedUser = undefined;
     if (authToken && claims) {
-      const fullUser = await findUserById(claims.authUser);
-      event.locals.authedUser = fullUser[0];
+      event.locals.authedUser = claims.authUser[0];
     }
   } finally {
     const response = await resolve(event);
