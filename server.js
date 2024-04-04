8d5c6d53985fe1507d550afb771dfcdaf2c37de7
Alef <alefff.gabriel@gmail.com>
2024-04-04 17:27:51 -0300

    fix: change cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // add a route that lives separately from the SvelteKit app
 // Set response header 'Access-Control-Allow-Origin : *'
-app.get('/myscript.js', cors({origin:'*'}));
+app.use('/api', cors());
 
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
