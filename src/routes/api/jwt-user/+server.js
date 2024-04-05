8011dd3bc97630afd0f271bf8c1eb13c564a4c5d
Alef <alefff.gabriel@gmail.com>
2024-04-05 20:02:59 -0300

    fix: add log on jwt route

--- src/routes/api/jwt-user/+server.js
+++ src/routes/api/jwt-user/+server.js
@@ -4,19 +4,14 @@ import { SECRET_INGREDIENT } from "$env/static/private";
 /** @type {import('@sveltejs/kit').Handle} */
 export async function POST({ request }) {
   const { authToken } = await request.json();
-  try {
-    const claims = jwt.verify(authToken, SECRET_INGREDIENT);
-    if (!claims) {
-      return new Response(JSON.stringify({ res: "error user not found" }), {
-        status: 404,
-      });
-    }
-    return new Response(JSON.stringify({ user: claims.authUser[0] }), {
-      status: 200,
-    });
-  } catch {
-    return new Response(JSON.stringify({ res: "error" }), {
-      status: 500,
+  const claims = jwt.verify(authToken, SECRET_INGREDIENT);
+  console.log(claims);
+  if (!claims) {
+    return new Response(JSON.stringify({ res: "error user not found" }), {
+      status: 404,
     });
   }
+  return new Response(JSON.stringify({ user: claims.authUser[0] }), {
+    status: 200,
+  });
 }
